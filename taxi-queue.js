2af08b04661f3cd3a7f48190bd3bb0b23056b87c
Avile Sindelo <avilesindelo@gmail.com>
2023-05-25 12:22:54 +0200

    Factory function and test cases

--- taxi-queue.js
+++ taxi-queue.js
@@ -1,28 +1,56 @@
-function TaxiQueue() {
+/*************** PSUEDOCODE ******************
+ * 
+ * Create a state variable with a passenger and taxi counts
+ * In the joinQueue function, increment the passenger count
+ * In the leaveQueue function, 
+ * 		Make sure the passenger count is not less than zero
+ * 			Decrement the passenger count
+ * In the joinTaxiQueue function, increment the taxi count of the state
+ * In the queueLength function, return the passenger count from the state
+ * In the taxiQueueLength function, return the taxi count from the state
+ * In the taxiDepart function, 	
+ * 		Make sure the passenger count is greater than or equal to 12
+ * 			Decrement the taxi count
+ * 			Remove 12 passengers from the passenger count of the state
+ * 
+ 
 
+*/
+
+
+function TaxiQueue() {
+	var state = {
+		passengers: 0,
+		taxis: 0
+	};
 
 	function joinQueue() {
-
+		state.passengers++;
 	}
 
 	function leaveQueue() {
-
+		if(state.passengers > 0){
+			state.passengers--;
+		}
 	}
 
 	function joinTaxiQueue() {
-
+		state.taxis++;
 	}
 
 	function queueLength() {
-
+		return state.passengers;
 	}
 
 	function taxiQueueLength() {
-
+		return state.taxis;
 	}
 
 	function taxiDepart(){
-
+		if(state.passengers >= 12){
+			state.taxis--;
+			state.passengers = state.passengers - 12;
+		}
 	}
 
 	return {
